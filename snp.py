15d4ac7eace99ae18880cadc77b8daaabfec0c8e
toxicface <47838537+toxicface@users.noreply.github.com>
2024-12-06 02:42:40 -0500

    Label creation for machine learning

--- snp.py
+++ snp.py
@@ -7,6 +7,7 @@ import yfinance as web
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
+import numpy as np
 import mplfinance as mpf
 import matplotlib
 
@@ -90,10 +91,33 @@ def graph_data():
 
     plt.show()
 
+def graph_corr():
+    df = pd.read_csv('sp500_joined_closes.csv')
+    df = df.iloc[:, 1:]
+    df_corr = df.corr()
+    df_corr = df.dropna(axis=1, how='all').corr()
+    data = df_corr.values
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    heatmap = ax.pcolor(data, cmap=plt.cm.RdYlGn)
+    fig.colorbar(heatmap)
+    ax.set_xticks(np.arange(data.shape[0]) + 0.5, minor=False)
+    ax.set_yticks(np.arange(data.shape[1]) + 0.5, minor=False)
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+    column_labels = df_corr.columns
+    row_labels = df_corr.index
+    ax.set_xticklabels(column_labels)
+    ax.set_yticklabels(row_labels)
+    plt.xticks(rotation=90)
+    heatmap.set_clim(-1,1)
+    plt.tight_layout()
+    plt.show()
 
 
-
-graph_data()
+#graph_data()
+graph_corr()
 #compile_data()
 print("Done")
 #get_data_from_yahoo()
